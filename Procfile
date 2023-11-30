ee395acf64fae565254967231109bbceb165e4b0
Oscar Armando <oscar@gmail.com>
2023-11-30 08:52:31 -0600

    decimo commit

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn 'betos.wsgi'
+release: python manage.py migrate
--- betos/settings.py
+++ betos/settings.py
@@ -134,10 +134,10 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'media')
 
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'static-only')
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 TEMPLATE_DIRS = [
